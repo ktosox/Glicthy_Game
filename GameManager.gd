ce37f2c04f14d961678624625257de8dd9c64bb3
Ktosox <ktoso3@gmail.com>
2019-06-17 19:41:25 +0200

    Added bullets / added a singleton GameManager (GM) node

---
+++ GameManager.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+var playerHP
+
+
+
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func lose_game():
+	pass
+
+
+func win_game():
+	pass
