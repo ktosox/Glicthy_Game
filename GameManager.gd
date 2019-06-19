5f9be6d0de52d19541b0ac6e487a292f75ebca72
Ktosox <ktoso3@gmail.com>
2019-06-19 22:15:11 +0200

    added Boss HP to overlay / connected player HP to overaly

--- GameManager.gd
+++ GameManager.gd
@@ -17,6 +17,7 @@ var pauseMenuScene = load("res://GameParts/PauseMenu/PauseMenu.tscn")
 var ganeOverMenuScene = load("res://GameParts/GameOverMenu/GameOverMenu.tscn")
 
 func _ready():
+
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Player/Overlay.gd
+++ Player/Overlay.gd
@@ -6,6 +6,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	updatePlayerHP(GM.playerHP)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,3 +14,19 @@ func _process(delta):
 	if($Curtain.modulate.a>0):
 		$Curtain.modulate.a -= 0.3 * delta
 	pass
+
+func updateBossHP(HP):
+	$VBoxContainer/Container/ProgressBar.value = HP
+	pass
+
+func showBossHP():
+	$VBoxContainer/Container/ProgressBar.visible = true
+	pass
+
+func hideBossHP():
+	$VBoxContainer/Container/ProgressBar.visible = false
+	pass
+
+
+func updatePlayerHP(HP):
+	$VBoxContainer/Container/Panel/CenterContainer/HBoxContainer/HP.text = String(HP)
